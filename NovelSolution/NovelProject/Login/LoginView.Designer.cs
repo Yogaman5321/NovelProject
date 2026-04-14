cacac6890bd4f513dd0ff33d51e839d37f9f4c30
Hunter Ballew <hunter421@ksu.edu>
2026-04-14 08:48:28 -0500

    Made simple Login UI

--- NovelSolution/NovelProject/Login/LoginView.Designer.cs
+++ NovelSolution/NovelProject/Login/LoginView.Designer.cs
@@ -28,19 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new System.Windows.Forms.TextBox();
+            textBox2 = new System.Windows.Forms.TextBox();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            button3 = new System.Windows.Forms.Button();
+            label5 = new System.Windows.Forms.Label();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(219, 194);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(212, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(219, 263);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(212, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            button1.Location = new System.Drawing.Point(35, 321);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(396, 41);
+            button1.TabIndex = 2;
+            button1.Text = "Log in with an account";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            button2.Location = new System.Drawing.Point(35, 510);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(396, 39);
+            button2.TabIndex = 3;
+            button2.Text = "Log in as a Guest";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            label1.Location = new System.Drawing.Point(159, 31);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(163, 62);
+            label1.TabIndex = 4;
+            label1.Text = "Login:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            label2.Location = new System.Drawing.Point(60, 194);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(91, 23);
+            label2.TabIndex = 5;
+            label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            label3.Location = new System.Drawing.Point(60, 264);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(86, 23);
+            label3.TabIndex = 6;
+            label3.Text = "Password:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            label4.Location = new System.Drawing.Point(219, 442);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(34, 23);
+            label4.TabIndex = 7;
+            label4.Text = "Or:";
+            // 
+            // button3
+            // 
+            button3.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            button3.Location = new System.Drawing.Point(35, 368);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(396, 41);
+            button3.TabIndex = 8;
+            button3.Text = "Create account with these credentials";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            label5.ForeColor = System.Drawing.Color.Red;
+            label5.Location = new System.Drawing.Point(100, 146);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(269, 20);
+            label5.TabIndex = 9;
+            label5.Text = "Error Label, change as needed in code";
+            label5.Visible = false;
             // 
             // LoginView
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(800, 450);
+            ClientSize = new System.Drawing.Size(472, 568);
+            Controls.Add(label5);
+            Controls.Add(button3);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             Name = "LoginView";
             Text = "LoginView";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
     }
 }
 
