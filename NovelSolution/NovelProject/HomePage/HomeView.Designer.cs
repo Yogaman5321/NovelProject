34c3bc01c44ae31e7781d10a32d7dd928a88094c
Zachery Haake-Lopez <zacheryhaake@ksu.edu>
2026-04-18 13:16:32 -0500

    setup for home page implementation

--- NovelSolution/NovelProject/HomePage/HomeView.Designer.cs
+++ NovelSolution/NovelProject/HomePage/HomeView.Designer.cs
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new System.Windows.Forms.Label();
+            WelcomeLabel = new System.Windows.Forms.Label();
             UxLastReadTB = new System.Windows.Forms.Label();
             UxLastReadListBox = new System.Windows.Forms.ListBox();
             SuspendLayout();
             // 
-            // label1
+            // WelcomeLabel
             // 
-            label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(84, 94);
-            label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(105, 20);
-            label1.TabIndex = 0;
-            label1.Text = "Welcome, user";
+            WelcomeLabel.AutoSize = true;
+            WelcomeLabel.Location = new System.Drawing.Point(52, 54);
+            WelcomeLabel.Name = "WelcomeLabel";
+            WelcomeLabel.Size = new System.Drawing.Size(105, 20);
+            WelcomeLabel.TabIndex = 0;
+            WelcomeLabel.Text = "Welcome, user";
             // 
             // UxLastReadTB
             // 
@@ -56,7 +56,7 @@
             UxLastReadListBox.FormattingEnabled = true;
             UxLastReadListBox.Location = new System.Drawing.Point(16, 213);
             UxLastReadListBox.Name = "UxLastReadListBox";
-            UxLastReadListBox.Size = new System.Drawing.Size(983, 344);
+            UxLastReadListBox.Size = new System.Drawing.Size(1142, 504);
             UxLastReadListBox.TabIndex = 2;
             // 
             // HomeView
@@ -65,16 +65,16 @@
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             Controls.Add(UxLastReadListBox);
             Controls.Add(UxLastReadTB);
-            Controls.Add(label1);
+            Controls.Add(WelcomeLabel);
             Name = "HomeView";
-            Size = new System.Drawing.Size(1020, 584);
+            Size = new System.Drawing.Size(1184, 740);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label WelcomeLabel;
         private System.Windows.Forms.Label UxLastReadTB;
         private System.Windows.Forms.ListBox UxLastReadListBox;
     }
